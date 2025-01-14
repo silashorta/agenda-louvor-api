4d5a6e156ea2e8fccd9e405241a458723eef27bb
silashorta <92862835+silashorta@users.noreply.github.com>
2025-01-14 00:40:48 -0300

    edit

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const cors = require("cors");
-const routes = require("../db/routes");
+const routes = require("./routes"); // Certifique-se de que este caminho está correto
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // Middleware para parsear JSON
 
-// Defina as rotas
+
 routes(app);
 
-// Exporte o servidor para o Vercel
+
+const porta = 3000;
+
+app.listen(porta, () => {
+  console.log("Rodando servidor na porta " + porta);
+});
+
 module.exports = app;
