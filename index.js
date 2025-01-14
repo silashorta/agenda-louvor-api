5a9a63cea9e164cf5e7d1e108ac919dc55f982b1
silashorta <92862835+silashorta@users.noreply.github.com>
2025-01-14 00:16:02 -0300

    up

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-const routes = require("../db/routes"); // Certifique-se de que este caminho está correto
+const routes = require("../db/routes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Defina as rotas
 routes(app);
 
-app.get("/", (req, res) => {
-  res.send("API está funcionando!");
-});
-
-// Não usamos app.listen aqui, pois o Vercel cuida da execução
+// Exporte o servidor para o Vercel
 module.exports = app;
